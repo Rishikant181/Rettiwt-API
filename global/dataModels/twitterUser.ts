f314ed5f9c98d1d40e85dd2128c32cceb2d435d3
dvnasutosh <71975246+dvnasutosh@users.noreply.github.com>
2022-01-29 17:27:55 +0530

    buffer

--- global/dataModels/twitterUser.ts
+++ global/dataModels/twitterUser.ts
@@ -3,14 +3,14 @@
 
 class UID{
     public ScreenName:any
-    private RestID:number
+    private RestID:number= 0
     public UserName:any
     public getRestID=()=>{ this.RestID }                    //Return Rest ID
 
 }
 
 class User{
-    _id : UID
+    _id : UID =new UID;
     relation = class{
         followers:[relativeUser]
         following:[relativeUser]
