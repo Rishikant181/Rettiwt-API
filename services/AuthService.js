3ed2c64930db9f0a57ce1b48fe07f8bec7adc3e6
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-01-29 14:07:21 +0530

    Addeed note fetch

--- services/AuthService.js
+++ services/AuthService.js
@@ -1,8 +1,8 @@
 // Package libs
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
 // This file contains the service that handles various function related to authentication and authorization
 
 export class AuthService {
-    
+
 };
