d48a2fd6841efdf2b1a36b8ed0b2825a027146c4
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-01-30 17:15:52 +0530

    Added UserAccountData type to schema

---
+++ src/schema/data/UserAccountData.ts
@@ -0,0 +1,31 @@
+// This file contains various object for handling data related to User Account
+
+// CUSTOM LIBS
+import { Deserializable } from "./Data";
+
+export class UserAccountDetails implements Deserializable {
+    // MEMBER DATA
+    __typename: string = '';                                                // To store the type of account
+    id: string = '';                                                        // To store the internal data id
+    screen_name: string = '';                                               // To store the screen name of the user
+    name: string = '';                                                      // To store the actual name of the user
+    created_at: string = '';                                                // To store the time when the account was created
+    description: string = '';                                               // To store the account description
+    verified: boolean = false;                                              // To store whether this is a verified account or not
+    favourites_count: number = 0;                                           // To store the number of favourites
+    followers_count: number = 0;                                            // To store the number of followers
+    friends_count: number = 0;                                              // To store the number of friends
+    statuses_count: number = 0;                                             // To store the number of status posted by user
+    location: string = '';                                                  // To store the user's location
+    media_count = 0;                                                        // To store the number of media posted by user
+    pinned_tweet_ids_str: string[] = [];                                    // To store the ids of tweets that are pinned in the account
+    profile_banner_url: string = '';                                        // To store the url to the profile's banner
+    profile_image_url_https: string = '';                                   // To store the url to the profile's image
+
+    
+    // Method to deserialize input data into current object
+    deserialize(data: any): this {
+        Object.assign(this, data);
+        return this;
+    }
+}
