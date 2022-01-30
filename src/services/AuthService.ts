6bd93ed906306166abd416920db4ab819b931dc9
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-01-30 11:22:40 +0530

    Removed unecessary headers from AuthService.generateGuestToken()

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -11,23 +11,8 @@ export class AuthService {
     scraps the guest token that is embbedded into the html page
     */
     generateGuestToken(url: string): Promise<string> {
-        // Preparing the headers
-        const headers: any = {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-language": "en-US,en;q=0.9",
-            "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Microsoft Edge\";v=\"97\", \"Chromium\";v=\"97\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"Windows\"",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "none",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1"
-        };
-
-        // Fetching the raw html from twitter
         return fetch(url, {
-            headers: headers,
+            headers: {},
             "referrerPolicy": "strict-origin-when-cross-origin",
             "body": null,
             "method": "GET"
