099efafb55a8951576d570b3bdb19d6154e9c94c
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-02-07 13:50:14 +0530

    A cursor is now taken as input by the function which generates the url to fetch tweets from

--- src/services/TweetService.ts
+++ src/services/TweetService.ts
@@ -25,7 +25,7 @@ export class TweetService {
         csrfToken: string,
         cookie: string
     ): Promise<Tweet[]> {
-        return fetch(userTweetsUrl(userId, numTweets), {
+        return fetch(userTweetsUrl(userId, numTweets, ''), {
             headers: authorizedHeader(authToken, csrfToken, cookie),
             body: null,
             method: "GET"
@@ -59,11 +59,39 @@ export class TweetService {
     getTweets(
         userId: string,
         numTweets: number,
+        cursor: string,
         authToken: string,
         csrfToken: string,
         cookie: string
     ): Promise <Tweet[]> {
-        
+        return fetch(userTweetsUrl(userId, numTweets, cursor), {
+            headers: authorizedHeader(authToken, csrfToken, cookie),
+            body: null,
+            method: "GET"
+        })
+        .then(res => res.json())
+        // Ignoring the next line because we still don't know the structure of response, so indexing it throws error
+        //@ts-ignore
+        .then(res => res['data']['user']['result']['timeline']['timeline']['instructions'][0]['entries'])
+        // Extracting required data from the tweets
+        .then(data => {
+            var tweets: Tweet[] = [];
+
+            //@ts-ignore
+            for(var i = 0; i < data.length - 2; i++) {
+                // Checking if actual tweet content is available
+                if(data[i]['content']['itemContent']) {
+                    const tweet = data[i]['content']['itemContent']['tweet_results']['result'];
+
+                    tweets.push(new Tweet().deserialize({
+                        'rest_id': tweet['rest_id'],
+                        ...tweet['legacy']
+                    }));
+                }
+            }
+
+            return tweets;
+        });
     }
 
     // Method to fetch tweets filtered by the supplied filter
