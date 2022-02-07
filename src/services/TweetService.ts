0cd81e601910edcfa28a6b56caac8f323cf84c7c
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-02-07 16:09:28 +0530

    Removed getRecentTweet function of TweetService

--- src/services/TweetService.ts
+++ src/services/TweetService.ts
@@ -17,44 +17,6 @@ import {
 
 export class TweetService {
     // MEMBER METHODS
-    // Method to fetch recent tweets made by a user using user id (rest_id)
-    getRecentTweets(
-        userId: string,
-        numTweets: number,
-        authToken: string,
-        csrfToken: string,
-        cookie: string
-    ): Promise<Tweet[]> {
-        return fetch(userTweetsUrl(userId, numTweets, ''), {
-            headers: authorizedHeader(authToken, csrfToken, cookie),
-            body: null,
-            method: "GET"
-        })
-        .then(res => res.json())
-        // Ignoring the next line because we still don't know the structure of response, so indexing it throws error
-        //@ts-ignore
-        .then(res => res['data']['user']['result']['timeline']['timeline']['instructions'][0]['entries'])
-        // Extracting required data from the tweets
-        .then(data => {
-            var tweets: Tweet[] = [];
-
-            //@ts-ignore
-            for(var i = 0; i < data.length - 2; i++) {
-                // Checking if actual tweet content is available
-                if(data[i]['content']['itemContent']) {
-                    const tweet = data[i]['content']['itemContent']['tweet_results']['result'];
-
-                    tweets.push(new Tweet().deserialize({
-                        'rest_id': tweet['rest_id'],
-                        ...tweet['legacy']
-                    }));
-                }
-            }
-
-            return tweets;
-        });
-    }
-
     // Method to fetch all tweets and replies made by a user
     getTweets(
         userId: string,
