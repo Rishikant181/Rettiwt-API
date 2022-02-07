701a547bc47d18f3471900c6a9ec247dfeefee8d
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-02-07 09:31:11 +0530

    Added cookie to env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,6 +4,7 @@ export const config = {
     },
     "twitter": {
         "root_url": "https://twitter.com/",
-        "authToken": "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
+        "authToken": "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+        "cookie": "des_opt_in=Y; kdt=J7xYhDLN2pqAgmT4k1fUDMIxOhgJSC2YhS6dBMaf; remember_checked_on=1; cd_user_id=17ae33d8c52317-019d4bb1b31a33-7e687a6e-100200-17ae33d8c53c47; dnt=1; G_ENABLED_IDPS=google; guest_id_marketing=v1%3A162834252695824370; guest_id_ads=v1%3A162834252695824370; mbox=PC#50e2e61bdf8e4e568d8bf362995a06c8.31_0#1707104868|session#975ba44c62db46c684d2292b40df5907#1643861928; g_state={\"i_l\":4,\"i_p\":1646387556944}; _sl=1; personalization_id=\"v1_yDLbMKYEfms5mR6rJu5wwg==\"; guest_id=v1%3A164420545031068193; gt=1490531810555559941; _twitter_sess=BAh7BiIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7AA%253D%253D--1164b91ac812d853b877e93ddb612b7471bebc74; auth_token=234d0f74dd1ef8fef361cf89c64ba3136658108c; ct0=eee23cc77c92c9cf32ed6f990ff2b41cd3cd5264b37e77f9d896b5968c4c367b1e6f68a2aec95bc81d2be6c92dcf4b997518eae1ed5e93dac630db73df342525b6dd4cd67b34049e9c44f49942ef35f4; twid=u%3D1490533552408043522"
     }
 }
