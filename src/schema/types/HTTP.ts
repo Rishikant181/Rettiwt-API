bd452cc9a05ad0782c1cd46db62a470ccae324f0
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-02-09 22:03:36 +0530

    Added Error object for http communication

--- src/schema/types/HTTP.ts
+++ src/schema/types/HTTP.ts
@@ -1,3 +1,16 @@
 // This file contains various objects for handling data related to HTTP communication
 
 // TODO: Add object to hold error response and make the services use this error type
+
+// Object to hold error data for http communication
+export class Error {
+    // MEMBER DATA
+    type: string;                                                           // To store the type of error
+    details: string;                                                        // To store additional details
+
+    // MEMBER METHODS
+    constructor(type: string, details: string) {
+        this.type = type;
+        this.details = details;
+    }
+}
