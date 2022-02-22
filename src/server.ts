9c6a9291aa5d49baab37083cc121449ed3cc2fae
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-02-22 15:30:22 +0530

    Removed unncessary AuthService and it's imports

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import express from 'express';
 // CUSTOM LIBS
 import { config } from './config/env';
 import { TweetFilter } from './schema/types/TweetData';
-import { AuthService } from './services/DataServices/AuthService';
 import { TweetService } from './services/DataServices/TweetService';
 import { UserAccountService } from './services/DataServices/UserAccountService';
 
