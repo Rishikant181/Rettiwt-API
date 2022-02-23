00d6bd22d99575f5f9b98741aaa0471360f962b2
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-02-23 13:25:55 +0530

    Added boilerplate Cache Service

--- src/services/CacheService.ts
+++ src/services/CacheService.ts
@@ -1,6 +1,23 @@
+// PACKAGE LIBS
+import { MongoClient } from "mongodb";
+
+// CUSTOM LIBS
+import { config } from '../config/env';
+
 /**
  * This service handles reading and writing of data from and to cache
  */
 export class CacheService {
-    
+    // MEMBER DATA
+    private client: MongoClient;                                        // To store the connection to mongodb database
+    private connUrl: string;                                            // To store the connection url
+
+    // MEMBER METHODS
+    constructor() {
+        // Initialising the connection url to database server
+        this.connUrl = `${config['server']['db']['host']}:${config['server']['db']['port']}`;
+
+        // Creating connection to database
+        this.client = new MongoClient(this.connUrl);
+    }
 }
