b508825c3a61cc2397862f85e7f2e67070d70219
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-02-24 08:31:49 +0530

    Removed unnecessary import from server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,6 @@ import express from 'express';
 
 // CUSTOM LIBS
 import { config } from './config/env';
-import { TweetFilter } from './schema/types/TweetData';
-import { CacheService } from './services/CacheService';
-import { TweetService } from './services/DataServices/TweetService';
-import { UserAccountService } from './services/DataServices/UserAccountService';
-import { findJSONKey } from './services/helper/Parser';
 
 // Initialising express instance
 const app = express();
