cd2e12fa0fb8d638f08283d4e1c09a1972626a94
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-02-24 09:51:25 +0530

    Added test code for dumping data to db

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import express from 'express';
 
 // CUSTOM LIBS
 import { config } from './config/env';
+import { TweetFilter } from './schema/types/TweetData';
+import { CacheService } from './services/CacheService';
+import { TweetService } from './services/DataServices/TweetService';
 
 // Initialising express instance
 const app = express();
@@ -13,6 +16,45 @@ app.use('/', (req, res) => {
 })
 
 // Setting up express server
-app.listen(config['server']['port'], () => {
+app.listen(config['server']['port'], async () => {
     console.log(`Listening on port ${config['server']['port']}`);
+
+    /* TEST CODE */
+    /*
+    var next;
+    var count = 0;
+
+    var service = new TweetService(
+        config['twitter']['auth']['authToken'],
+        config['twitter']['auth']['csrfToken'],
+        config['twitter']['auth']['cookie']
+    );
+
+    var cache = new CacheService();
+
+    while(true) {
+        var res = await service.getTweets(new TweetFilter({
+            words: [],
+            hashtags: [],
+            fromUsers: ['@1canw1n'],
+            toUsers: [],
+            mentions: [],
+            startDate: '',
+            endDate: '',
+            count: 20
+        }), next);
+
+        if(res.success) {
+            cache.write(res.data.tweets);
+            next = res.data.next;
+            count += res.data.tweets.length;
+            console.log(`${res.success}\t\t${count}\t\t${res.data.tweets.at(-1).createdAt}`);
+        }
+        else {
+            console.log("Done");
+            break;
+        }
+    }
+    */
+    /* TEST CODE END */
 });
