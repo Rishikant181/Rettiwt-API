d3886c7ff370662f450f7c6c53247e6cc73ad3d5
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-03-03 14:09:27 +0530

    Fixed indentation in server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,23 +7,23 @@ import { TweetFilter } from './schema/types/TweetData';
 import { CacheService } from './services/CacheService';
 import { TweetService } from './services/DataServices/TweetService';
 import { UserAccountService } from './services/DataServices/UserAccountService';
-import { schema }  from './schema/graphql/queries/RootQuery';
+import { schema } from './schema/graphql/queries/RootQuery';
 
 //ANCHOR: GraphQL test import
 import GraphiQL from 'graphiql';
-import {buildSchema} from 'graphql'
-import {graphqlHTTP} from 'express-graphql'
+import { buildSchema } from 'graphql'
+import { graphqlHTTP } from 'express-graphql'
 // Initialising express instance
 const app = express();
 
-app.use('/graphql',graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
 // Creating root end point
 app.use('/', (req, res) => {
-    res.send("Hello World");    
+    res.send("Hello World");
 });
 
 // Setting up express server
