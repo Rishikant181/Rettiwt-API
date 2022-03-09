12ad1fe2ac7f6fd31fa3ad81e8acf5ebab467af5
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-03-09 20:14:36 +0530

    Tweets query not returns any number of tweets requested

--- src/schema/graphql/resolvers/TweetSpecific.ts
+++ src/schema/graphql/resolvers/TweetSpecific.ts
@@ -17,8 +17,34 @@ var tweetService = new TweetService(
  * @returns The list of tweets matchin the given filter
  */
 export async function resolveTweets(filter: any): Promise<any> {
-    // Getting the data
-    var res = (await tweetService.getTweets(new TweetFilter(filter), '')).data.tweets;
+    var tweets: any[] = [];                                                     // To store the list of tweets
+    var next: string = '';                                                      // To store cursor to next batch
+    var total: number = 0;                                                      // To store the total number of tweets fetched
+    var batchSize: number = 20;                                                 // To store the batchsize to use
 
-    return res;
+    // Preparing the filter to use
+    const tweetFilter = new TweetFilter(filter);
+
+    // If required count less than batch size, setting batch size to required count
+    batchSize = (tweetFilter.count < batchSize) ? tweetFilter.count : batchSize;
+    
+    // Repeatedly fetching data as long as total data fetched is less than requried
+    while(total < tweetFilter.count) {
+        // If this is the last batch, change batch size to number of remaining tweets
+        batchSize = ((tweetFilter.count - total) < batchSize) ? (tweetFilter.count - total) : batchSize;
+
+        // Getting the data
+        const res = (await tweetService.getTweets(tweetFilter, next)).data;
+
+        // Adding fetched tweets to list of tweets
+        tweets = tweets.concat(res.tweets);
+
+        // Updating total tweets fetched
+        total += res.tweets.length;
+
+        // Getting cursor to next batch
+        next = res.next
+    }
+
+    return tweets;
 }
