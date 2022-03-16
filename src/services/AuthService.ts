cf62a7336101a666c0eb6ae6107d8878e88df4da
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-03-16 12:58:31 +0530

    getAuthCredentials method of AuthService now returns the next available crential with each subsequent request

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -13,14 +13,20 @@ export class AuthService {
         csrfToken: string,
         cookie: string
     };                                                                       // To store the current authentication credentials
+    private numCredentials: number;                                          // To store the total number of available credentials
+    private credNumber: number;                                              // To keep track of the current credential's number
 
     // MEMEBER METHODS
     private constructor() {
         // Initializing member data
         this.requestCount = 0;
 
+        // Initializing the total number of available credentials
+        this.numCredentials = config['twitter']['auth'].length;
+
         // Initializing authentication credentials to the first available one
         this.authCredentials = config['twitter']['auth'][0];
+        this.credNumber = 0;
     }
 
     /**
@@ -37,13 +43,25 @@ export class AuthService {
     }
 
     /**
-     * @returns The current authentication credentials
+     * @summary Changes to current active credential to the next available
+     */
+    private changeCredentials(): void {
+        // Checking if more credentials are availabe and changing current credentials' number
+        this.credNumber = (this.credNumber == (this.numCredentials - 1)) ? 0 : (this.credNumber + 1);
+
+        // Changing the current credential
+        this.authCredentials = config['twitter']['auth'][this.credNumber];
+    }
+
+    /**
+     * @returns The current authentication credentials. A different credential is returned each time this is invoked
      */
     getAuthCredentials(): {
         authToken: string,
         csrfToken: string,
         cookie: string
     } {
+        this.changeCredentials();        
         return this.authCredentials;
     }
 }
