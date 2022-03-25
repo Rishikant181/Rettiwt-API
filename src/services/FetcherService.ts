0693d59324e6e0f8c556a394e9d8a75b07584861
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-03-25 16:06:12 +0530

    Caching can now be turned of using the corresponding setting in env.ts

--- src/services/FetcherService.ts
+++ src/services/FetcherService.ts
@@ -16,6 +16,9 @@ import {
     authorizedHeader
 } from './helper/Requests'
 
+// CONFIG
+import { config } from '../config/env';
+
 /**
  * @summary Stores all the different type of http requests
  */
@@ -44,7 +47,14 @@ enum HttpStatus {
  * @service The base serivice from which all other data services derive their behaviour
  */
 export class FetcherService {
+    // MEMBER DATA
+    private allowCache: boolean;                                            // To store whether caching is enabled or not
+
     // MEMBER METHODS
+    protected constructor() {
+        this.allowCache = config['server']['db']['enabled'];
+    }
+
     /**
      * @returns The absolute raw json data from give url
      * @param url The url to fetch data from
@@ -61,14 +71,26 @@ export class FetcherService {
             method: method ? method : HttpMethods.GET,
             body: body
         })
-        // Checking http status
-        .then(res => this.handleHTTPError(res))
-        // Parsing data to json
-        .then(res => res.json())
-        // If other unknown error
-        .catch((err) => {
-            throw err;
-        });
+            // Checking http status
+            .then(res => this.handleHTTPError(res))
+            // Parsing data to json
+            .then(res => res.json())
+            // If other unknown error
+            .catch((err) => {
+                throw err;
+            });
+    }
+
+    /**
+     * @summary Throws the appropriate http error after evaluation of the status code of reponse
+     * @param res The response object received from http communication
+     */
+     private handleHTTPError(res: Response): Response {
+        if (res.status != 200 && res.status in HttpStatus) {
+            throw new Error(HttpStatus[res.status])
+        }
+
+        return res;
     }
 
     /**
@@ -76,18 +98,21 @@ export class FetcherService {
      * @param data The extracted data to be cached
      */
     protected async cacheData(data: any): Promise<void> {
-        // Creating an instance of cache
-        var cache = new CacheService();
+        // If caching is enabled
+        if (this.allowCache) {
+            // Creating an instance of cache
+            var cache = new CacheService();
 
-        // Parsing the extracted data
-        //@ts-ignore
-        var users = data.users.map(user => new User().deserialize(user));
-        //@ts-ignore
-        var tweets = data.tweets.map(tweet => new Tweet().deserialize(tweet));
+            // Parsing the extracted data
+            //@ts-ignore
+            var users = data.users.map(user => new User().deserialize(user));
+            //@ts-ignore
+            var tweets = data.tweets.map(tweet => new Tweet().deserialize(tweet));
 
-        // Caching the data
-        cache.write(users);
-        cache.write(tweets);
+            // Caching the data
+            cache.write(users);
+            cache.write(tweets);
+        }
     }
 
     /**
@@ -95,22 +120,13 @@ export class FetcherService {
      * @param id The id of the data to be read from cache
      */
     protected async readData(id: string): Promise<any> {
-        // Creating an instance of cache
-        var cache = new CacheService();
+        // If caching is enabled
+        if (this.allowCache) {
+            // Creating an instance of cache
+            var cache = new CacheService();
 
-        // Reading data from cache
-        return cache.read(id);
-    }
-
-    /**
-     * @summary Throws the appropriate http error after evaluation of the status code of reponse
-     * @param res The response object received from http communication
-     */
-    private handleHTTPError(res: Response): Response {
-        if (res.status != 200 && res.status in HttpStatus) {
-            throw new Error(HttpStatus[res.status])
+            // Reading data from cache
+            return cache.read(id);
         }
-
-        return res;
     }
 }
