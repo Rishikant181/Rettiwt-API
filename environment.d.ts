e02deae8d171d9bd23401b80e6d12609b7f58ccd
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-04-18 13:59:18 +0000

    Used environment variables instead of env.ts for host and port configurations

--- environment.d.ts
+++ environment.d.ts
@@ -2,9 +2,9 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            CACHE_DB_HOST: string,
-            CACHE_DB_PORT: number,
-            APP_PORT: number
+            CACHE_DB_HOST: string,                              // To store the hostname of the caching database
+            CACHE_DB_PORT: number,                              // To store the port number of caching database
+            APP_PORT: number                                    // To store the port on which this app runs
         }
     }
 }
