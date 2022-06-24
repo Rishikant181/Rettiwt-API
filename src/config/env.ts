65fafb0eb8745945ceccf89fe17d699f20b972d2
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-06-24 20:22:28 +0530

    Added urls for different endpoints of various apis and services

--- src/config/env.ts
+++ src/config/env.ts
@@ -16,7 +16,6 @@ export const config = {
         }
     },
     twitter: {
-        root_url: "https://twitter.com/",
         auth: {
             authToken: "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
         }
--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,10 +1,18 @@
 // This file contains the various urls used throughout the app
 
 /**
+ * @summary Stores the urls used for communication with twitter api
+ */
+export const twitter_urls = {
+    root: 'https://twitter.com/'                                            // The twitter root url
+};
+
+/**
  * @summary Stores the urls used for communication with core API
  */
-export const core = {
-    getCookie: (id: string) => `/Cookies/fetch/${id}`,                      // Endpoint to get a cookie with the given id
-    getAllCookies: '/Cookies/fetch',                                        // Endpoint to get all the stored cookies
-    addCookie: '/addCookie'                                                 // Endpoint to store a given cookie
+export const core_urls = {
+    root: `${process.env.CORE_HOST}:${process.env.CORE_PORT}`,              // The core API root url
+    single_cookie: (id: string) => `/Cookies/fetch/${id}`,                  // Endpoint to get a cookie with the given id
+    all_cookies: '/Cookies/fetch',                                          // Endpoint to get all the stored cookies
+    add_cookie: '/addCookie'                                                // Endpoint to store a given cookie
 };
