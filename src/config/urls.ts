165bbb0018b394ce6901acdab2e2818a92fc42e1
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-06-27 23:55:31 +0530

    Update core urls

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -14,5 +14,5 @@ export const core_urls = {
     root: `http://${process.env.CORE_HOST}:${process.env.CORE_PORT}`,       // The core API root url
     single_cookie: (id: string) => `${core_urls.root}/Cookies/fetch/${id}`, // Endpoint to get a cookie with the given id
     all_cookies: () => `${core_urls.root}/Cookies/fetch`,                   // Endpoint to get all the stored cookies
-    add_cookie: () => `${core_urls.root}/addCookie`                         // Endpoint to store a given cookie
+    add_cookie: () => `${core_urls.root}/Cookies/add`                       // Endpoint to store a given cookie
 };
