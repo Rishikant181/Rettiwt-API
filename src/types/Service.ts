bcf305ff644be85cc3f7078984408deb2639a0ad
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-06-30 22:36:23 +0530

    Replaced string cursor with 'Cursor' object cursor in all data services' return type

--- src/types/Service.ts
+++ src/types/Service.ts
@@ -1,7 +1,11 @@
+export type Cursor = {
+    value: string;                                                      // To store the cursor to next batch of data
+}
+
 /**
  * @summary Stores cursored data that is returned by services
  */
 export type CursoredData<Type> = {
     list: Type[];                                                       // To store the list data
-    next: string;                                                       // To store the cursor to the next batch
+    next: Cursor;                                                       // To store the information about cursor to the next batch
 }
