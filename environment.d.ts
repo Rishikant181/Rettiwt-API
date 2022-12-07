20fc87db71e2ec906aa9aa4a613377a1d43c39a1
Rishikant Sahu <rishikantsahu181@gmail.com>
2022-12-07 21:14:07 +0530

    Made urls more straightforward

--- environment.d.ts
+++ environment.d.ts
@@ -4,12 +4,8 @@ declare global {
         interface ProcessEnv {
             USE_CACHE: boolean;                                 // To store whether to use cache or not
             STORE_LOGS: boolean;                                // To store whether to use logs or not
-            CACHE_DB_HOST: string,                              // To store the hostname of the caching database
-            CACHE_DB_PORT: number,                              // To store the port number of caching database
-            DATA_DB_HOST: string,                               // To store the hostname of the data db
-            DATA_DB_PORT: number,                               // To store the port number of the data db
-            CORE_HOST: string,                                  // To store the hostname of the core api
-            CORE_PORT: number,                                  // To store the port number of the core api
+            CACHE_DB_URL: string,                               // To store the url of the caching database
+            DATA_DB_URL: string,                                // To store the url of the mongodb database
             APP_PORT: number                                    // To store the port on which this app runs
         }
     }
