c0a699fbfe14ec2454e9e4e82436c48c29653bce
Rishikant181 <rishikantsahu181@gmail.com>
2023-01-21 16:58:51 +0530

    Removed unnecessary env vars

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,5 @@ RUN npm install
 COPY . .
 RUN npm run build
 ENV DEVELOPMENT=false
-ENV USE_CACHE=false
 ENV APP_PORT=3000
-ENV CACHE_DB_URL=redis://cache:6379
 CMD npm run start
--- environment.d.ts
+++ environment.d.ts
@@ -3,9 +3,6 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             DEVELOPMENT: string;                                // To store whether environment is production or development
-            USE_CACHE: string;                                  // To store whether to use cache or not
-            CACHE_DB_URL: string,                               // To store the url of the caching database
-            DATA_DB_URL: string,                                // To store the url of the mongodb database
             APP_PORT: string                                    // To store the port on which this app runs
         }
     }
