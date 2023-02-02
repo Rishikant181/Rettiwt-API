b7541372f9153575df4a8e0a3123605e69e12f2a
Rishikant181 <rishikantsahu181@gmail.com>
2023-02-02 20:41:49 +0530

    Added wrapper API

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// SERVICES
+import { AuthService } from "./services/AuthService";
+import { UserAccountService } from "./services/data/UserAccountService";
+import { TweetService } from "./services/data/TweetService";
+
+/**
+ * @param cookie The cookies string to use to fetch data
+ * @returns The API for fetching user and tweet data
+ */
+const Rettiwt = (cookie: string) => {
+    // Creating new auth service instance using the given cookie string
+    const auth: AuthService = new AuthService(cookie);
+
+    // Using the auth service instance to create data services instances
+    return {
+        users: new UserAccountService(auth),
+        tweets: new TweetService(auth)
+    };
+}
+
+export default Rettiwt;
