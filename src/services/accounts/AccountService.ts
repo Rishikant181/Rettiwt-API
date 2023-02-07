973a6d2e5daff85dc87d1c2800d7596d25d2e27a
Rishikant181 <rishikantsahu181@gmail.com>
2023-02-07 20:13:42 +0530

    Fixed storing of cookies after each flow

--- src/services/accounts/AccountService.ts
+++ src/services/accounts/AccountService.ts
@@ -10,18 +10,19 @@ import { GuestCredentials } from '../../types/Authentication';
 // HELPERS
 import LoginFlows from './LoginFlows';
 import { loginHeader } from '../helper/Headers';
+import { Cookie, CookieJar } from 'cookiejar';
 
 export class AccountService {
     // MEMBER DATA
     private auth: AuthService;                                                  // To store the auth service instance to use
     private guestCreds: GuestCredentials;                                       // To store the guest credentials to use
-    private cookie: string;                                                     // To store the cookies received
+    private cookies: Cookie[];                                                  // To store the cookies received from twitter
 
     // MEMBER METHODS
     constructor() {
         this.auth = new AuthService();
         this.guestCreds = { authToken: '', guestToken: '' };
-        this.cookie = '';
+        this.cookies = [];
     }
 
     /**
@@ -43,14 +44,32 @@ export class AccountService {
     private async initiateLogin(): Promise<CurlyResult> {
         // Initiating the login process
         return await curly.post(LoginFlows.InitiateLogin.url, {
-            httpHeader: loginHeader(await this.getGuestCredentials(), this.cookie),
+            httpHeader: loginHeader(await this.getGuestCredentials(), this.cookies.toString()),
             sslVerifyPeer: false,
             postFields: ''
         });
     }
 
+    private async jsInstrumentationSubtask(): Promise<CurlyResult> {
+        // Initiating login
+        const res: CurlyResult = await this.initiateLogin();
+
+        // Storing cookies received
+        this.cookies = new CookieJar().setCookies(res.headers[0]['Set-Cookie'] as string[]);
+
+        // Getting the flow token from previous flow
+        let flowToken: string = res.data['flow_token'];
+
+        // Executing the flow
+        return await curly.post(LoginFlows.JsInstrumentationSubtask.url, {
+            httpHeader: loginHeader(await this.getGuestCredentials(), this.cookies.join(';').toString()),
+            sslVerifyPeer: false,
+            postFields: JSON.stringify(LoginFlows.JsInstrumentationSubtask.body(flowToken))
+        });
+    }
+
     public async login(email: string, userName: string, password: string) {
-        this.initiateLogin().then(res => {
+        this.jsInstrumentationSubtask().then(res => {
             console.log(res);
         });
     }
