c8d26968a93a3b125da55ece4b45ee9cba896790
Dvain84143 <dvnasutosh@gmail.com>
2023-02-09 03:09:18 +0530

    added trend data extractor

--- src/services/helper/Extractors.ts
+++ src/services/helper/Extractors.ts
@@ -5,10 +5,15 @@ import RawUserFollowers from '../../types/raw/user/Followers';
 import RawUserFollowing from '../../types/raw/user/Following';
 import RawUserLikes from '../../types/raw/user/Likes';
 import RawUserTweets from '../../types/raw/user/Tweets';
+
 import RawTweet from '../../types/raw/tweet/Tweet';
 import RawTweets from '../../types/raw/tweet/Tweets';
 import RawLikers from '../../types/raw/tweet/Favouriters';
 import RawRetweeters from '../../types/raw/tweet/Retweeters';
+
+import RawTrends from '../../types/raw/general/Trends';
+import {Item} from '../../types/raw/general/Trends';
+import Trends, { trendEntry } from '../../types/Trends';
 
 // PARSERS
 import * as Parsers from './Parser';
@@ -452,4 +457,31 @@ export function extractTweetReplies(res: RawTweet, tweetId: string): {
         users: users,
         tweets: tweets
     };
+}
+
+export function extractTrendData(res:RawTrends):Trends {
+    
+    // Setting Initial values
+    let currentTrends:Trends={
+        trendingNow:[],
+        trendCategory: res.pageConfiguration.tabs.initialTabId,
+    }
+
+    // Fetching each Trend and its data and push it to currentTrends.trendingNow
+    res.timeline.instructions[1].addEntries?.entries[1].content.timelineModule?.items.forEach(entry=>{
+        const trend:trendEntry={
+            name: entry.item.content.trend.name,
+            rank: Number(entry.item.content.trend.rank),
+            
+            //Getting approx amount of tweets done if it exists in the metaData 
+            approxtweetCount : Parsers.convertToNumber(entry.item.content.trend.trendMetadata.metaDescription??''),
+
+            relatedTopics : entry.item.clientEventInfo.details.guideDetails.transparentGuideDetails.trendMetadata.associatedCuratedTweetIds,
+            domainContext : entry.item.content.trend.trendMetadata.domainContext
+
+        }
+        currentTrends.trendingNow.push(trend)
+    })
+    
+    return currentTrends
 }
