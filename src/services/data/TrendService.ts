c70ff761d2159b7384c95efb0bf03ab418ef2df1
Dvain84143 <dvnasutosh@gmail.com>
2023-02-09 03:28:35 +0530

    Added tweet Services - doc included WARNING- To be Tested

--- src/services/data/TrendService.ts
+++ src/services/data/TrendService.ts
@@ -1,17 +1,36 @@
-import Trends from "../../types/raw/tweet/Trends";
+
 import { AuthService } from "../AuthService";
 import { FetcherService } from "../FetcherService";
 
+// TYPES
+import Trends, { trendOn } from "../../types/Trends";
+import RawTrends from "../../types/raw/general/Trends";
+
 // URLS
 import * as Urls from '../helper/Urls';
+import { extractTrendData } from "../helper/Extractors";
+
+/**
+ * A class that handles trend-related functionalities.
+ *
+ * @class TrendService
+ * @extends FetcherService
+ *
+ * @param {AuthService} auth - An instance of the `AuthService` class.
+ *
+ * @property {(type: trendOn) => Promise<Trends>} getTrends - A method to retrieve trends based on the specified type.
+ */
 
 export class TrendService extends FetcherService{
     constructor(auth: AuthService) {
         super(auth);
     }
 
-    async getTrends(): Promise<Trends> {
-        let res = await this.request<RawTrends>(Urls.trendsUrl()).then(res => res.data);
-        return res;
+    async getTrends(type:trendOn): Promise<Trends> {
+        let res = await this.request<RawTrends>(Urls.trendsUrl(type));
+        
+        let Data = extractTrendData(res.data)
+        this.cacheData(Data);
+        return Data;
     }
 }
