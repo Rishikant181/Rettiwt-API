e557c4e8f800eea9a4dc049c8d1a20a2707eeb48
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-03-28 16:20:36 +0530

    AuthCookie can now be initialized as empty

--- src/models/auth/AuthCookie.ts
+++ src/models/auth/AuthCookie.ts
@@ -8,25 +8,27 @@ import { DataValidationError } from '../errors/DataValidationError';
 export class AuthCookie implements IAuthCookie {
     /** Token used to authenticate a device. */
     @IsNotEmpty()
-    kdt: string;
+    kdt: string = '';
 
     /** Token used to authenticate a user using a Twitter ID. */
     @IsNotEmpty()
-    twid: string;
+    twid: string = '';
 
     /** The CSRF token of the session. */
     @IsNotEmpty()
-    ct0: string;
+    ct0: string = '';
 
     /** The authentication token used while logging in to the account. */
     @IsNotEmpty()
-    auth_token: string;
+    auth_token: string = '';
 
-    constructor(cookie: IAuthCookie) {
-        this.auth_token = cookie.auth_token;
-        this.ct0 = cookie.ct0;
-        this.kdt = cookie.kdt;
-        this.twid = cookie.twid;
+    constructor(cookie?: IAuthCookie) {
+        if (cookie) {
+            this.auth_token = cookie.auth_token;
+            this.ct0 = cookie.ct0;
+            this.kdt = cookie.kdt;
+            this.twid = cookie.twid;
+        }
 
         // Validating the cookie
         const validationResult = validateSync(this);
