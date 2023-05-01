843dceb11662d2b092371e39235256a2eacacedf
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-05-01 21:32:51 +0530

    Constructor of Variables now initializes conditionally, the required parameters

--- src/requests/payloads/Variables.ts
+++ src/requests/payloads/Variables.ts
@@ -1,6 +1,16 @@
 // TYPES
 import { IVariables } from '../../types/Query';
 
+/**
+ * The different types of resources that can be fetched.
+ */
+export enum ResourceType {
+    'TWEET_DETAILS',
+    'TWEET_REPLIES',
+    'TWEET_LIKES',
+    'TWEET_RETWEETS'
+};
+
 export class Variables implements IVariables {
     tweetId?: string;
     focalTweetId?: string;
@@ -24,24 +34,25 @@ export class Variables implements IVariables {
     withV2Timeline: boolean = true;
 
     /**
-     * Generates the query variables object for fetching tweet details.
+     * Initializes the appropriate Variables object based on the requred resource type and parameters.
      * 
-     * @param focalTweetId The id of the target tweet.
-     * 
-     * @returns The query variable object.
+     * @param resourceType The type of resource that is requested.
+     * @param params The additional paramters for fetching the resource.
      */
-    tweetDetail(focalTweetId: string): Variables {
-        // Setting necessary parameters
-        this.focalTweetId = focalTweetId;
-
-        // Removing unnecessary paramteters
-        delete this.tweetId;
-        delete this.userId;
-        delete this.screen_name;
-        delete this.count;
-        delete this.cursor;
-
-        return this;
+    constructor(resourceType: ResourceType, params: { id?: string, count?: number, cursor?: string }) {
+        if (resourceType == ResourceType.TWEET_DETAILS) {
+            this.focalTweetId = params.id;
+        }
+        else if (resourceType == ResourceType.TWEET_REPLIES) {
+            this.focalTweetId = params.id;
+            this.count = params.count;
+            this.cursor = params.cursor;
+        }
+        else if (resourceType == (ResourceType.TWEET_LIKES || ResourceType.TWEET_RETWEETS)) {
+            this.tweetId = params.id;
+            this.count = params.count;
+            this.cursor = params.cursor;
+        }
     }
 
     /**
