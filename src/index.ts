1687bcd4eaee8a52835f1cfd408622018cd352e4
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-07-05 16:33:44 +0530

    Used DataValidationError from rettiwt-core

--- src/index.ts
+++ src/index.ts
@@ -51,13 +51,14 @@ export * from './types/Rettiwt';
 export * from './types/Service';
 export * from './types/Tweet';
 export * from './types/User';
+export { IDataValidationError } from 'rettiwt-core';
 
 // Exporting models
 export { TweetFilter } from 'rettiwt-core';
 export * from './models/args/TweetListArgs';
 export * from './models/args/UserListArgs';
-export * from './models/auth/AuthCookie';
+export { AuthCookie } from './models/auth/AuthCookie';
 export * from './models/data/CursoredData';
 export * from './models/data/Tweet';
 export * from './models/data/User';
-export * from './models/errors/DataValidationError';
+export { DataValidationError } from 'rettiwt-core';
