f5bc6a2e6ea66e96db1690abce8ba0e74aae3fc6
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-07-11 12:01:03 +0530

    Removed graphql server

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,3 @@
 export const config = {
-    port: Number(process.env.APP_PORT),
-    is_development: (process.env.DEVELOPMENT == 'true'),
     twitter_auth_token: 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
 }
