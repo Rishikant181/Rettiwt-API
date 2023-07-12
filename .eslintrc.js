84104d8f2a1a1fa63f8537f94b86ce0779142ade
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-07-12 14:11:43 +0530

    Added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint/eslint-plugin'],
+	extends: ['plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
+	root: true,
+	env: {
+		node: true,
+		jest: true,
+	},
+	ignorePatterns: ['.eslintrc.js'],
+	rules: {
+		'@typescript-eslint/naming-convention': [
+			'warn',
+			{
+				selector: ['class'],
+				format: ['PascalCase'],
+			},
+			{
+				selector: 'interface',
+				format: ['PascalCase'],
+				prefix: ['I'],
+			},
+			{
+				selector: 'enum',
+				format: ['PascalCase'],
+				prefix: ['E'],
+			},
+			{
+				selector: ['variableLike', 'memberLike'],
+				format: ['camelCase'],
+			},
+			{
+				selector: 'enumMember',
+				format: ['UPPER_CASE'],
+			},
+		],
+		'@typescript-eslint/explicit-function-return-type': 'error',
+		'@typescript-eslint/explicit-module-boundary-types': 'error',
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'@typescript-eslint/no-extraneous-class': [
+			'warn',
+			{
+				allowEmpty: true,
+			},
+		],
+		'@typescript-eslint/no-inferrable-types': 'off',
+	},
+};
