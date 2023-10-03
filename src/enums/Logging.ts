4c988308856d8ebff342ecedd3983629efb6d937
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-10-03 18:14:36 +0530

    Added different log actions

---
+++ src/enums/Logging.ts
@@ -0,0 +1,10 @@
+/**
+ * The different types of actions that are logged.
+ *
+ * @internal
+ */
+export enum ELogActions {
+	HTTP_REQUEST = 'HTTP_REQUEST',
+	EXTRACT = 'EXTRACT',
+	DESERIALIZE = 'DESERIALIZE',
+}
