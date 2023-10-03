83a4bcf2751c8970dd2f7bda54da76cbfd582d2a
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-10-03 17:27:17 +0530

    Added blank LogService

---
+++ src/services/LogService.ts
@@ -0,0 +1,13 @@
+/**
+ * Handles logging of data for debug purpose.
+ *
+ * @internal
+ */
+export class LogService {
+	/** Whether logging is enabled or not. */
+	private readonly enabled: boolean;
+
+	constructor() {
+		this.enabled = true;
+	}
+}
