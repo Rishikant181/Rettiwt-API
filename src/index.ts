6bcfc92c7680adbd025f1ba7404ab2253819c9a8
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-10-06 00:29:27 +0530

    Updated exports

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ export * from './Rettiwt';
 // Exporting enums
 export * from './enums/ApiErrors';
 export * from './enums/HTTP';
+export * from './enums/Logging';
 
 // Exporting models
+export * from './models/internal/RettiwtConfig';
 export * from './models/public/CursoredData';
 export * from './models/public/List';
 export * from './models/public/Tweet';
@@ -13,10 +15,12 @@ export * from './models/public/User';
 
 // Exporting services
 export * from './services/internal/FetcherService';
+export * from './services/internal/LogService';
 export * from './services/public/TweetService';
 export * from './services/public/UserService';
 
 // Exporting types
+export * from './types/internal/RettiwtConfig';
 export * from './types/public/CursoredData';
 export * from './types/public/List';
 export * from './types/public/Tweet';
