5a3f3fc64e25231a053a0325d997cc50f874265b
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-11-21 20:27:21 +0530

    Added sub-command for fetching tweet details

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@
 // PACKAGES
 import { Command } from 'commander';
 import { Rettiwt } from '.';
+import { output } from './helper/CliUtils';
 
 // Creating a new commandline program
 const program = new Command();
@@ -13,17 +14,32 @@ program.name('rettiwt').description('A CLI tool for fetching data from Twitter f
 // Initializing Rettiwt instance
 const rettiwt = new Rettiwt();
 
-/**
- * USER commands
- */
+// Creating parent commands to structure sub-commands
 const user = program.command('user').description('Command for accessing resources releated to users');
+const tweet = program.command('tweet').description('Command for accessing resources releated to tweets');
 
-user.command('details')
-	.argument('<username>', 'The username of the Twitter user whose details are to be fetched')
-	.action(async (username: string) => {
-		const details = await rettiwt.user.details(username);
-		console.log(details);
-	});
+// Sub-commands for 'tweet'
+{
+	// Details
+	tweet
+		.command('details')
+		.argument('<id>', 'The id of the tweet whose details are to be fetched')
+		.action(async (id: string) => {
+			const details = await rettiwt.tweet.details(id);
+			output(details);
+		});
+}
+
+// Sub-commands for 'user'
+{
+	// Details
+	user.command('details')
+		.argument('<id>', 'The username/id of the Twitter user whose details are to be fetched')
+		.action(async (id: string) => {
+			const details = await rettiwt.user.details(id);
+			output(details);
+		});
+}
 
 // Finalizing the CLI
 program.parse();
