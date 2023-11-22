d2bba38e2bdc3df1af4a4bc933c0b4da8220625a
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-11-22 13:45:23 +0530

    Modified output method of CliUtils

--- src/cli.ts
+++ src/cli.ts
@@ -12,7 +12,7 @@ const program = new Command();
 program.name('rettiwt').description('A CLI tool for fetching data from Twitter for free!');
 
 // Initializing Rettiwt instance
-const rettiwt = new Rettiwt();
+const rettiwt = new Rettiwt({ apiKey: process.env.API_KEY });
 
 // Creating parent commands to structure sub-commands
 const user = program.command('user').description('Command for accessing resources releated to users');
@@ -28,6 +28,22 @@ const tweet = program.command('tweet').description('Command for accessing resour
 		.action(async (id: string) => {
 			const details = await rettiwt.tweet.details(id);
 			output(details);
+		});
+
+	// Search
+	tweet
+		.command('search')
+		.description('Fetch the list of tweets that match the given filter options')
+		.argument('[count]', 'The number of items to fetch')
+		.argument('[cursor]', 'The cursor to the batch of items to fetch')
+		.option('-f, --from <string>')
+		.action(async (count?: string, cursor?: string, options?: { from: string }) => {
+			const tweets = await rettiwt.tweet.search(
+				{ fromUsers: options?.from.split(';') },
+				count ? parseInt(count) : undefined,
+				cursor,
+			);
+			output(tweets);
 		});
 }
 
