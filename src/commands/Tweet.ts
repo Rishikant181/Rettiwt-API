334e914f5d886590ff1f2b8547d31ced5e8f69ec
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-12-11 12:22:40 +0530

    Changed structure for commands

--- src/commands/Tweet.ts
+++ src/commands/Tweet.ts
@@ -1,9 +1,7 @@
 // PACKAGES
 import { Command, createCommand } from 'commander';
 import { Rettiwt } from '../Rettiwt';
-
-// MODELS
-import { TweetSearchOptions } from './options/TweetSearchOptions';
+import { TweetFilter } from 'rettiwt-core';
 
 // UTILITY
 import { output } from '../helper/CliUtils';
@@ -116,6 +114,50 @@ function createTweetCommand(rettiwt: Rettiwt): Command {
 		});
 
 	return tweet;
+}
+
+/**
+ * The search options supplied while searching for tweets.
+ *
+ * @remarks The search options are implementations of the ones offered by {@link TweetFilter}
+ */
+class TweetSearchOptions {
+	public from?: string;
+	public to?: string;
+	public words?: string;
+	public hashtags?: string;
+	public start?: string;
+	public end?: string;
+
+	/**
+	 * Initializes a new object from the given options.
+	 *
+	 * @param options - The search options.
+	 */
+	public constructor(options?: TweetSearchOptions) {
+		this.from = options?.from;
+		this.to = options?.to;
+		this.words = options?.words;
+		this.hashtags = options?.hashtags;
+		this.start = options?.start;
+		this.end = options?.end;
+	}
+
+	/**
+	 * Converts the filter options to a format recognizable by rettiwt-api.
+	 *
+	 * @returns The '{@link TweetFilter}' representation of filter options.
+	 */
+	public toTweetFilter(): TweetFilter {
+		return new TweetFilter({
+			fromUsers: this.from ? this.from.split(';') : undefined,
+			toUsers: this.to ? this.to.split(';') : undefined,
+			words: this.words ? this.words.split(';') : undefined,
+			hashtags: this.hashtags ? this.hashtags.split(';') : undefined,
+			startDate: this.start ? new Date(this.start) : undefined,
+			endDate: this.end ? new Date(this.end) : undefined,
+		});
+	}
 }
 
 export default createTweetCommand;
