b885b440d1443f5cc74b2488f94f792a1eb264b1
Rishikant Sahu <rishikantsahu181@gmail.com>
2024-01-25 18:00:36 +0530

    Added docs for max size of media that can be posted

--- src/types/args/TweetMediaArgs.ts
+++ src/types/args/TweetMediaArgs.ts
@@ -4,7 +4,11 @@
  * @public
  */
 export interface ITweetMediaArgs {
-	/** The path to the media file */
+	/**
+	 * The path to the media file.
+	 *
+	 * @remarks The size of the media file must be \<= 5242880 bytes.
+	 */
 	path: string;
 
 	/** The list usernames of users to be tagged in the media. */
