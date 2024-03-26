004713ad3f20da20c897b1147a11fc9a6262214f
Rishikant Sahu <rishikantsahu181@gmail.com>
2024-03-26 11:30:44 +0530

    Formatted code

--- .eslintrc.js
+++ .eslintrc.js
@@ -122,7 +122,7 @@ module.exports = {
 		'import/order': [
 			'warn',
 			{
-				groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+				groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
 				alphabetize: {
 					order: 'asc',
 					caseInsensitive: true,
